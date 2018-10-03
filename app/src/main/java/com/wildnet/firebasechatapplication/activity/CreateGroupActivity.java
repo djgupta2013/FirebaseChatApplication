dbfb87b1f341ba0d2310107f8e1e2f3dd6c856ae
djgupta2013 <djgupta2013@gmail.com>
2018-10-03 15:25:29 +0530

    initial
    Signed-off-by: djgupta2013 <djgupta2013@gmail.com>

--- app/src/main/java/com/wildnet/firebasechatapplication/activity/CreateGroupActivity.java
+++ app/src/main/java/com/wildnet/firebasechatapplication/activity/CreateGroupActivity.java
@@ -54,7 +54,6 @@ public class CreateGroupActivity extends AppCompatActivity implements NewGroupCr
         recyclerView = (RecyclerView) findViewById(R.id.recycler_Newgroup);
     }
 
-
     private void getAllUser() {
         DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
         rootRef.child("User").addValueEventListener(new ValueEventListener() {
